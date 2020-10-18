6a9685959478a63cf9433ce0487822d5d55d4d89
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-10-18 01:52:36 -0500

    more...

--- AlgorithmExamples/Program.cs
+++ AlgorithmExamples/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AlgorithmExamples
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
@@ -20,6 +22,20 @@ namespace AlgorithmExamples
             Console.WriteLine("---fibonacci double recursive---");
             const int fibonacciValue = 9;
             Console.WriteLine(FibonacciRecursive(fibonacciValue));
+
+            Console.WriteLine("---Get the largest number in the array---");
+            var arrayOfNumbers = new[] {1, 30, 34, 60, 2, 55};
+            Console.WriteLine(GetBiggest(arrayOfNumbers));
+
+            Console.WriteLine("---Flatten array---");
+            var nestedArray = new[]
+            {
+                new object[]{ new object[] { 4,5,6, new object[] { 6,7,8} } },
+                new object[] {2, 3},
+                new object[] {4, 5, 6}
+            };
+            Console.WriteLine(nestedArray);
+            Console.WriteLine(FlattenArray(nestedArray));
         }
 
         private static Dictionary<string, int> RepeatedWordCounter(string text)
@@ -40,6 +56,33 @@ namespace AlgorithmExamples
         {
             if (number <= 1) return 1;
             return FibonacciRecursive(number - 1) + FibonacciRecursive(number - 2);
+        }
+
+        private static int GetBiggest(IEnumerable<int> arr) => arr.Aggregate((acc, el) => acc > el ? acc : el);
+
+        private static object[] FlattenArray(object jaggedArray)
+        {
+            var flattenedArray = new List<object>();
+            var jaggedArrayType = jaggedArray.GetType();
+            var expectedType = typeof(int);
+
+            if (jaggedArrayType.IsArray)
+            {
+                if (expectedType.IsAssignableFrom(jaggedArrayType.GetElementType()))
+                    flattenedArray.AddRange((object[]) jaggedArray);
+                else
+                    flattenedArray.AddRange(((object[]) jaggedArray).SelectMany(FlattenArray));
+            }
+            else if (jaggedArrayType == expectedType)
+            {
+                flattenedArray.Add((int) jaggedArray);
+            }
+            else
+            {
+                return new object[0];
+            }
+
+            return flattenedArray.ToArray();
         }
     }
 }
